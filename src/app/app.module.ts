5a6972f74407dd70cfe551b9d4b66e2d8200dbb1
michael.lehman <mike.lehman11@gmail.com>
2018-11-01 12:58:23 -0400

    start here branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { DataService } from './services/data-service';
-import { HttpClientModule } from '@angular/common/http';
-import { Form, FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
+    BrowserModule
   ],
-  providers: [
-    DataService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
