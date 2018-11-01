7d114d03f3a126b5376515a2a7cc2610d9224756
michael.lehman <mike.lehman11@gmail.com>
2018-11-01 12:52:11 -0400

    starting point branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data-service';
-import { Brewery } from './models/brewery-model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  protected loading: boolean = false;
-  protected msg: string;
-  protected name: string;
-  protected state: string;
-  protected limit: string = "10";
-  protected breweries: Brewery[] = [];
-
-  constructor(private dataService: DataService) { }
-
-  public ngOnInit(): void {
-    this.getAllBreweries();
-  }
-
-  private getAllBreweries(): void {
-    this.dataService.Get()
-      .subscribe(data => {
-        this.breweries = data;
-      });
-  }
-
-  private clear(): void {
-    this.name = "";
-  }
-
-  private onSubmit(): void {
-    this.loading = true;
-
-    this.dataService.GetWithParams(this.state, this.state, this.limit)
-      .subscribe(data => {
-        this.breweries = data;
-        this.loading = false;
-      });
-  }
+export class AppComponent {
+  title = 'app';
 }
